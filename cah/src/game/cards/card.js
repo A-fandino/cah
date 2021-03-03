7aaf31d282424aa2c72a847d63dd9f7557876eb6
A-fandino <arnauf8@gmail.com>
2021-03-03 14:08:07 +0100

    Create game button style

--- cah/src/game/cards/card.js
+++ cah/src/game/cards/card.js
@@ -4,20 +4,22 @@ import style from "./styles";
 
 export default class Card extends Component {
   render() {
-    let divClass = "card "+this.props.color+"-card";
+    let divClass = "card " + this.props.color + "-card";
     let hideClass = "";
     let imgClass = "";
-    if (this.props.color === "white" && this.props.children === "") {
+    if (this.props.color === "white" && !this.props.children) {
       hideClass = " hide";
     }
+    console.log("Content", this.props.children);
     if (this.props.color === "black") {
-    imgClass = "invert";
-  }
+      imgClass = "invert";
+    }
+
     return (
       <div style={style.cards} className={divClass + hideClass}>
         <span className="card-header">{this.props.children}</span>
         <div className="card-bottom">
-          <img className = {imgClass}style={style.img} src={icon} alt="|"/>
+          <img className={imgClass} style={style.img} src={icon} alt="|" />
           <span>{this.props.set}</span>
         </div>
       </div>
