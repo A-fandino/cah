22ace7339e7be3ca04977be23ebb0647afc7b237
A-fandino <arnauf8@gmail.com>
2021-03-05 10:25:41 +0100

    Multiple white card implemented (WIP)

--- cah/src/game/cardView.js
+++ cah/src/game/cardView.js
@@ -1,42 +1,51 @@
 import React, { Component } from "react";
 import Card from "./cards/card";
 import gameAccess from "./accessFb";
-import Cookies from "universal-cookie";
+//import Cookies from "universal-cookie";
 
 class CardView extends Component {
   constructor() {
     super();
     this.state = {
-      whiteName: "",
-      whiteSet: "",
       blackName: "",
       blackSet: "",
+      whiteCards: [],
     };
   }
 
   componentDidMount() {
-    const cookies = new Cookies();
+    //const cookies = new Cookies();
     const whiteCard = gameAccess({
       gameId: this.props.game,
-      color: "white",
-      player: cookies.get("id"),
     });
-
+    let cardsObj = [];
+    let blackCard = [];
     whiteCard.on("value", (snapshot) => {
-      this.setState({
-        whiteName: snapshot.child("card").val(),
-        whiteSet: snapshot.child("set").val(),
-      });
-    });
+      const values = snapshot.val();
+      for (let key in values) {
+        const curr = values[key];
+        if (key !== "blackCard") {
+          cardsObj.push([key, curr.card, curr.set]);
+        } else {
+          blackCard = [curr.text, curr.set];
+        }
+      }
 
-    const blackCard = gameAccess({ gameId: this.props.game, color: "black" });
+      this.setState({
+        blackName: blackCard[0],
+        blackSet: blackCard[1],
+        whiteCards: cardsObj,
+      });
+      console.log(this.state.blackName, blackCard);
+    });
+    /*const blackCard = gameAccess({ gameId: this.props.game, color: "black" });
 
     blackCard.on("value", (snapshot) => {
       this.setState({
         blackName: snapshot.child("text").val(),
         blackSet: snapshot.child("set").val(),
       });
-    });
+    }); */
   }
   render() {
     return (
@@ -44,9 +53,17 @@ class CardView extends Component {
         <Card set={this.state.blackSet} color="black">
           {this.state.blackName}
         </Card>
-        <Card set={this.state.whiteSet} color="white">
+        {/* <Card set={this.state.whiteSet} color="white">
           {this.state.whiteName}
-        </Card>
+        </Card> */}
+
+        {this.state.whiteCards.map((card) => {
+          return (
+            <Card key={card[0]} set={card[2]} color="white">
+              {card[1]}
+            </Card>
+          );
+        })}
       </React.Fragment>
     );
   }
