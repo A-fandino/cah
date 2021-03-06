312155aa86e4e03ed0b273dde88c5939a6840b98
A-fandino <arnauf8@gmail.com>
2021-03-06 13:46:34 +0100

    Real time cards multiplayer

--- cah/src/game/cardView.js
+++ cah/src/game/cardView.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Card from "./cards/card";
 import gameAccess from "./accessFb";
-//import Cookies from "universal-cookie";
 
 class CardView extends Component {
   constructor() {
@@ -14,38 +13,33 @@ class CardView extends Component {
   }
 
   componentDidMount() {
-    //const cookies = new Cookies();
+    let cardsObj;
     const whiteCard = gameAccess({
       gameId: this.props.game,
     });
-    let cardsObj = [];
     let blackCard = [];
     whiteCard.on("value", (snapshot) => {
+      cardsObj = [];
       const values = snapshot.val();
       for (let key in values) {
         const curr = values[key];
         if (key !== "blackCard") {
-          cardsObj.push([key, curr.card, curr.set]);
+          cardsObj.push(
+            <Card key={key} set={curr.set} color="white">
+              {curr.card}
+            </Card>
+          );
         } else {
           blackCard = [curr.text, curr.set];
         }
       }
-
+      console.log(cardsObj);
       this.setState({
         blackName: blackCard[0],
         blackSet: blackCard[1],
         whiteCards: cardsObj,
       });
-      console.log(this.state.blackName, blackCard);
     });
-    /*const blackCard = gameAccess({ gameId: this.props.game, color: "black" });
-
-    blackCard.on("value", (snapshot) => {
-      this.setState({
-        blackName: snapshot.child("text").val(),
-        blackSet: snapshot.child("set").val(),
-      });
-    }); */
   }
   render() {
     return (
@@ -53,17 +47,7 @@ class CardView extends Component {
         <Card set={this.state.blackSet} color="black">
           {this.state.blackName}
         </Card>
-        {/* <Card set={this.state.whiteSet} color="white">
-          {this.state.whiteName}
-        </Card> */}
-
-        {this.state.whiteCards.map((card) => {
-          return (
-            <Card key={card[0]} set={card[2]} color="white">
-              {card[1]}
-            </Card>
-          );
-        })}
+        {this.state.whiteCards.map((card) => card)}
       </React.Fragment>
     );
   }
