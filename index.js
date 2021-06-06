5ae9c3fa374e3238fd7be335ddd9151d0def92f9
A-fandino <arnauf8@gmail.com>
2021-06-06 21:22:17 +0200

    Heroku env port

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import Express from "express";
 import path from "path";
 import apiRoute from "./routes/api.js";
-import userRoute from "./routes/user.js";
-import gameRoute from "./routes/game.js";
-import mongoose from "mongoose";
+// import userRoute from "./routes/user.js";
+// import gameRoute from "./routes/game.js";
+// import mongoose from "mongoose";
 import dotenv from "dotenv";
 let __dirname = path.resolve(path.dirname(""));
 const app = Express();
@@ -18,18 +18,20 @@ app.use(Express.static(path.join(__dirname, "client/build")));
 
 const port = 5000;
 dotenv.config();
-mongoose.connect(
-  process.env.CONNECTION,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => console.log("Connected to mongoose ඞ")
-);
+// mongoose.connect(
+//   process.env.CONNECTION,
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   () => console.log("Connected to mongoose ඞ")
+// );
 
 app.use("/api", apiRoute);
-app.use("/user", userRoute);
-app.use("/game", gameRoute);
+// app.use("/user", userRoute);
+// app.use("/game", gameRoute);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-app.listen(port, () => console.log("Listening on port " + port));
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Listening on port " + port)
+);
