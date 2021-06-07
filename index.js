472908fa6378cbfeee2bc8566dcccfe110617def
A-fandino <arnauf8@gmail.com>
2021-06-07 12:38:37 +0200

    Server started message updated

--- index.js
+++ index.js
@@ -31,6 +31,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log("Listening on port " + port)
-);
+app.listen(process.env.PORT || 5000, () => console.log("Server started"));
